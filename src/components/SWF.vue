f44cf1b6902355ba63aca455cca1d48e833e16f2
mattstryfe <purebias@gmail.com>
2018-05-13 20:40:26 -0400

    all http requests complete
    google and both wgov requests have been completed.

--- src/components/SWF.vue
+++ src/components/SWF.vue
@@ -12,23 +12,25 @@
               placeholder="ex: 20170"
               v-model="userZip"
               required
-
             ></v-text-field>
           </v-form>
         </v-flex>
       </v-layout>
     </v-container>
-    <span>zipcode: {{ userZip }}</span>
+    <span>zipcode: {{ this.locDetails }}</span>
   </div>
 </template>
 
 <script>
+  import * as Vue from 'vue-resource'
+
   export default {
     name: 'SWF',
 		drawerToggle: false,
     data: () => ({
       userZip: '',
       title: 'Simple Weather Forecast (SWF)',
+      locDetails: '',
     }),
 		props:['zip'],
 		created: function () {
@@ -37,11 +39,42 @@
 		},
     methods: {
       resolveLocation () {
-      	console.log('userZip', this.userZip)
-        Vue.http.get('https://maps.googleapis.com/maps/api/geocode/json?address=', this.userZip).then(response => {
-        	this.body = response.body;
-        	console.log('response body:', this.body);
-        })
+        const config = {
+      	  apiKey: 'AIzaSyBoGMPpAjvF8DhxSEeQ80QObwx6ARnoTxE',
+          geoLocUrl: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + this.userZip,
+          wGov: {
+            baseUrl: 'https://api.weather.gov/points/',
+            fullUrl: '',
+            gridUrl: ''
+          }
+        }
+        this.$http.get(config.geoLocUrl, config).then(res => {
+          const locDetails = {
+            geo : {
+              lat: res.body.results[0].geometry.location.lat,
+              lng: res.body.results[0].geometry.location.lng
+            },
+        	  zipcode: res.body.results[0].address_components[0].short_name,
+            city: res.body.results[0].address_components[1].short_name,
+            county: res.body.results[0].address_components[2].short_name,
+            state: res.body.results[0].address_components[3].short_name,
+            country: res.body.results[0].address_components[4].short_name,
+          }
+
+          // make available to app
+          this.locDetails = locDetails;
+
+          // build wGov link
+          config.wGov.fullUrl = config.wGov.baseUrl + locDetails.geo.lat + ',' + locDetails.geo.lng
+
+          return this.$http.get(config.wGov.fullUrl, config);
+        }).then(function(WgovResponse) {
+            console.log('first wGov res', WgovResponse)
+            config.wGov.gridUrl = WgovResponse.body.properties.forecastGridData;
+            this.$http.get(config.wGov.gridUrl, config).then (res => {
+              console.log('final wGov res', res);
+            })
+          })
       }
     }
   }
