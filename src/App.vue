04708a5d823827f40ab090db4a7c5071210f672b
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-10-09 19:31:22 -0400

    saving.

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,31 @@
 <template>
-  <v-app id="inspire">
-
-    <side-bar :drawer="drawer"></side-bar>
-
-    <v-toolbar
-      color="blue darken-3"
-      dark
-      app
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      fixed
-    >
-      <v-toolbar-title>
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <router-link :to="'/'">
-          <span class=" white--text">Project Bluefire</span>
-        </router-link>
-
-      </v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <!-- search bar -->
-      <v-text-field
-        disabled
-        flat
-        solo-inverted
-        prepend-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon disabled>
-        <v-icon>apps</v-icon>
-      </v-btn>
-
-      <v-btn icon disabled>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-
-    </v-toolbar>
-
-    <!-- MAIN CONTENT -->
-    <v-content>
-      <router-view/>
-    </v-content>
-
-    <!-- Bootom: located in common/ -->
-    <Bottom></Bottom>
-  </v-app>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import Bottom from './common/Bottom'
-import SideBar from './common/SideBar'
-
-export default {
-  name: 'App',
-  components: {SideBar, Bottom},
-  props: {
-    source: String
-  },
-  data () {
-    return {
-      drawer: false,
-      dialog: false
-    }
-  },
-  created () {
-
-  },
-  methods: {
-
-  }
-}
-</script>
-
 <style>
-#inspire {
+#app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-  a {
-    text-decoration: none
+#nav {
+  padding: 30px;
+}
 
-  }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,20 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
-import './assets/weatherIcons/css/weather-icons.min.css'
-import moment from 'moment'
+import store from './store'
 
 Vue.config.productionTip = false
-// make moment available to entire app
-Vue.prototype.moment = moment
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
