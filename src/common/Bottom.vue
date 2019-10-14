8f30f08ac5a7e92671cbe7a904365f0e5b61ce1d
Matthew Esterberg <purebias@gmail.com>
2019-10-14 14:08:54 -0400

    layout
    main layout changes have been completed.  working on populating the main page with some content.

--- src/common/Bottom.vue
+++ src/common/Bottom.vue
@@ -1,22 +1,18 @@
 <template>
-  <v-footer app inset>
-    <v-container fluid class="pa-0 ma-0">
-      <v-row class="justify-center align-center pa-0 ma-0 no-gutters">
-        <v-col cols="6" class="pa-0 ma-0">
-          &copy;2019
-          <strong>Project Bluefire</strong>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            icon
-            class="mx-3 white--text"
-          >
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
-          <a href="https://buttercms.com"><img class="butter" src="./../assets/butter-w.png" align="middle"></a>
-        </v-col>
-      </v-row>
-    </v-container>
+  <v-footer app inset padless>
+    <v-col cols="12" class="pa-0 ma-0 text-center">
+      &copy;2019
+      <strong>Project Bluefire</strong>
+      <v-btn
+        v-for="icon in icons"
+        :key="icon"
+        icon
+        class="mx-3 white--text"
+      >
+        <v-icon size="24px">{{ icon }}</v-icon>
+      </v-btn>
+      <a href="https://buttercms.com"><img class="butter" src="./../assets/butter-w.png" align="middle"></a>
+    </v-col>
   </v-footer>
 </template>
 
