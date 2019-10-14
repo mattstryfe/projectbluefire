eedc83ea80c51bde7388f0c9cee7d4416d9199a8
Matthew Esterberg <purebias@gmail.com>
2019-10-14 14:22:49 -0400

    home page
    home page redesign is finished enough to publish.  moving onto blog.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-<!--    <img alt="Vue logo" src="../assets/logo.png">-->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row class="text-center mb-5">
+      <v-col>
+        <h1>Project Bluefire</h1>
+        <span>{{ missionStatement }}</span>
+      </v-col>
+    </v-row>
+
+    <v-divider></v-divider>
+
+    <v-row class="justify-space-around mt-5">
+      <v-col cols="4" v-for="item in items" :key="item.icon" :to="item.href" class="text-center">
+        <v-card flat color="transparent" class="pa-2">
+          <v-icon size="8vw" :color="item.color" >{{ item.icon}}</v-icon>
+          <v-card-text class="title font-weight-bold">{{ item.title }}</v-card-text>
+          <v-card-text class=" pt-0">{{ item.desc }}</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'home',
+    data () {
+      return {
+        title: 'Home Page',
+        missionStatement: 'An attempt to improve everything; beginning with weather.',
+        items: [
+          {icon: 'fa-cloud-download-alt', title: 'SWF', href: 'SWF', color: 'blue darken-2', desc: 'Simple Weather Forecast (SWF). A simple daily forecast.'},
+          {icon: 'fa-tools', title: 'Projects', href: 'Projects', color: 'orange darken-2', desc: 'Small group of side projects including, peltiers, DHT Sensors, and ESP8266 modules.'},
+          {icon: 'fa-comment-dots', title: 'Blog', href: 'Blog', color: 'green darken-2', desc: 'Capturing the new build, day-by-day. Also some ideas and current events.'}
+        ]
+      }
+    }
   }
-}
 </script>
+
+<style scoped>
+</style>
