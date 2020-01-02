5fe79c7fec41b4ae844aee8924a8643bd1b21dfd
Matthew Esterberg <purebias@gmail.com>
2020-01-02 16:27:18 -0500

    added plattsburgh
    added some  test data which has both rain and snow.  this should help with styling and properly configuring/testing the snow totals

--- src/views/SWF.vue
+++ src/views/SWF.vue
@@ -20,6 +20,15 @@
       >
         get Test Data
       </v-btn>
+
+      <v-btn
+        class="ml-3"
+        color="secondary"
+        small
+        @click="generatePlattsburgh()"
+      >
+        Plattsburgh
+      </v-btn>
     </v-row>
 
     <!-- CARDs -->
@@ -41,6 +50,7 @@ import {weatherGovAPI, googleGeoLocAPI} from '@/services/SWFServices'
 import dayjs from 'dayjs'
 import { testData } from "../assets/data/testData";
 import ForecastCard from "../components/ForecastCard/ForecastCard";
+import { plattsburghTestData } from "../assets/data/plattsburghData";
 
 export default {
   name: "SWF",
@@ -52,6 +62,12 @@ export default {
       user_lng: null,
       raw_weather: null,
       finalWeatherData: null,
+      plattsburgh: {
+        geo: {
+          lat: 44.7479,
+          lng: -73.4417
+        }
+      },
       test_loc_details: {
         geo: {
           lat: 38.8629803,
@@ -90,7 +106,7 @@ export default {
   computed: {},
   watch: {},
   methods: {
-    processWeatherData2(rawWeatherData, targetProps) {
+    processWeatherData(rawWeatherData, targetProps) {
       console.log('rawWeatherData', rawWeatherData.properties)
 
       // ------ Helper Functions --- //
@@ -177,115 +193,18 @@ export default {
       }
       return masterObj
     },
-    processWeatherData(rawWeatherData, targetProps) {
-      console.log('rawWeatherData', rawWeatherData.properties)
-
-      function removePHP(val) {
-        const newVal = val.validTime.split('/')
-        return newVal[0]
-      }
-
-      function generateArrayOfDates(duration) {
-        let dateArr = []
-        let today = moment()
-
-        for (let i=0; i <= duration; i++)
-          dateArr.push(today.clone().add(i, 'days').utc().format('YYYY-MM-DD'))
-
-        return dateArr
-      }
-
-      class PropBuilder {
-        constructor(date, rawWeatherData) {
-          // this[date] = this.getData(date, rawWeatherData.properties)
-          // this.filtered = this.filteredData(date, rawWeatherData.properties)
-          this.appendProps = Object.fromEntries(targetProps.map(prop => [ prop, {} ] ))
-          // this.buildDateObj = this.buildThings(date)
-          // this.data = this.getData(date, rawWeatherData.properties)
-        }
-        buildThings(date) {
-          // return {[date]: Object.fromEntries(withTheseProps.map(prop => [ prop, {} ] ))}
-        }
-        getData(date, rawData) {
-          let tmpObj = {}
-          for (let [key, val] of Object.entries(rawData)) {
-            if (targetProps.includes(key)) {
-              tmpObj[key] = val
-            }
-          }
-          return tmpObj
-        }
-        filteredData (date, rawData) {
-          let tmpObj = {}
-          for (let [key, val] of Object.entries(rawData)) {
-            if (targetProps.includes(key)) {
-              // TODO find a way to group data as we iterate over it
-              let valsByDate = val.values.filter(value => moment(removePHP(value)).utc().format('YYYY-MM-DD') === date )
-              tmpObj[key] = valsByDate
-            }
-          }
-          return tmpObj
-        }
-      }
-      // const today = new DateWeather('2019-12-23')
-      // console.log('today', today)
-
-      function buildMasterObj () {
-        let props = new PropBuilder()
-        let masterObj = {}
-
-        generateArrayOfDates(5).forEach((date) => {
-          masterObj[date] = props.appendProps
-        })
-        return masterObj
-      }
-
-      let masterObj = buildMasterObj()
-
-      console.log('masterObj', masterObj)
-
-      // class Thing {
-      //   constructor() {
-      //     // this.props = Object.fromEntries(withTheseProps)
-      //     this.y = 3.14;
-      //   }
-      //   f(aString) {
-      //     console.log('f ran!', aString)
-      //   }
-      //   g() {}
-      // }
-      // const o = new Thing();
-
-      // Build base object first
-
-      // TODO Working copy
-      // let strippedWeatherData = {}
-      // for (let [key, val] of Object.entries(rawWeatherData.properties)) {
-      //   if (targetedProps.includes(key)) {
-      //     strippedWeatherData[key] = val
-      //   }
-      //
-      // }
-      //
-      // let dateObj = {}
-      // dates.forEach((date) => {
-      //   let categoryObj = {}
-      //   for (let [key, val] of Object.entries(strippedWeatherData)) {
-      //     let valsByDate = val.values.filter(value => moment(removePHP(value)).utc().format('YYYY-MM-DD') === date )
-      //     categoryObj[key] = valsByDate
-      //     count += 1
-      //   }
-      //   dateObj[date] = categoryObj
-      // })
-      // console.log('dateObj after date: ', dateObj)
-    },
     getTestData() {
       let t0 = performance.now()
 
-      // this.finalWeatherData = this.processWeatherData(testData, this.withTheseProps)
-      this.finalWeatherData = this.processWeatherData2(testData, this.withTheseProps)
+      this.finalWeatherData = this.processWeatherData(testData, this.withTheseProps)
       console.log('finalWeatherData', this.finalWeatherData)
 
+      let t1 = performance.now();
+      console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to generate.');
+    },
+    generatePlattsburgh() {
+      let t0 = performance.now()
+      this.finalWeatherData = this.processWeatherData(plattsburghTestData, this.withTheseProps)
       let t1 = performance.now();
       console.log('Took', (t1 - t0).toFixed(4), 'milliseconds to generate.');
     },
@@ -296,7 +215,8 @@ export default {
           weatherGovAPI
             .get(res.data.properties.forecastGridData)
             .then(res => {
-              this.finalWeatherData = this.processWeatherData2(res.data, this.withTheseProps)
+              console.log('res', res)
+              this.finalWeatherData = this.processWeatherData(res.data, this.withTheseProps)
               // TODO remove php date interval
             })
         })
