91edd53b145e2cd2cc47b00c3680ce7476bae17e
Matthew Esterberg <purebias@gmail.com>
2020-06-29 01:50:29 -0400

    added timline items and styling

--- src/store.js
+++ src/store.js
@@ -5,6 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    positions: [
+      {
+        year: 2020,
+        company: 'MAXAR',
+        text_color: 'yellow--text',
+        title: 'Front End Developer',
+        tech: ['Vue.js', 'VUEX', 'Vuetify'],
+        highlight: 'Led & executed ground up development of Vue.js App.',
+        details: 'Cultivated and deployed stakeholder vision from concept to reality < 8 months during pandemic.'
+      },
+      {
+        year: 2019,
+        company: 'MAXAR',
+        text_color: 'yellow--text',
+        title: 'Front End Developer',
+        tech: ['Vue.js', 'Bootstrap', 'AngularJS'],
+        highlight: 'Executed rebuild of AngularJS App.',
+        details: 'Built and deployed Vue.js & Micronaut App.' +
+          ' Constructed from the ground up. ' +
+          'First of its kind and virtually no templates, guides, or pre configuration files.'
+      },
+      {
+        year: 2018,
+        company: 'Invictus',
+        icon: '@/assets/images/portfolio/invictus-logo.png',
+        text_color: 'blue-grey--text text--lighten-5',
+        title: 'Front End Developer',
+        tech: ['Vue.js', 'Leaflet', 'Turf.js', 'AngularJS'],
+        highlight: ' Stand-in Lead for multiple sprint cycles.',
+        details: 'Built and integrated Leaflet powered interactive map component.' +
+          'Introduced real-time geospatial storm tracking and personnel checks within alert areas.'
+      },
+      {
+        year: 2017,
+        company: 'Invictus',
+        title: 'Front End Developer',
+        tech: ['AngularJS'],
+        highlight: 'Implemented full-state support across application.',
+        details: 'Built & Integrated user history, word cloud, summary window, and text comparison tool'
+      },
+      {
+        year: 2016,
+        company: 'U.S.A.F',
+        title: 'Front End Developer',
+        tech: ['AngularJS'],
+        highlight: 'Implemented full-state support across application.',
+        details: 'Built & Integrated user history, word cloud, summary window, and text comparison tool'
+      },
+    ],
     drawerToggle: true,
     pages: [
       {
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -39,13 +39,49 @@
           <p><span class="amber--text text--darken-2">Email:</span> Yes.</p>
           <p><span class="amber--text text--darken-2">Phone:</span> Yes.</p>
           <p><span class="amber--text text--darken-2">Discord:</span> @mattstryfe2161</p>
-
         </v-col>
       </section>
 
       <!-- Where I've been -->
-      <section id="where-ive-been" class="row pa-5 mx-0 pos-rel">
+      <section id="where-ive-been" class="row pa-5 mx-0 pos-rel mt-10">
+        <v-col cols="12" class=" text-right">
+          <span class="display-2 amber--text text--darken-2">
+            Where I've been
+          </span>
+        </v-col>
+
         <v-col cols="12">
+          <v-timeline>
+            <v-timeline-item large color="#343536"
+              v-for="(position, i) in positions" :key="i">
+
+              <template v-slot:opposite>
+                <span class="subtitle-2">{{ position.year }}</span>
+              </template>
+
+              <template v-slot:icon>
+                <span class="subtitle-2" :class="position.text_color">{{ position.company }}</span>
+              </template>
+
+              <v-card class="elevation-2 c-border-a">
+                <v-card-title class="headline">
+                  <span class="blue--text text--lighten-2 text-caption mx-1"> | </span>
+                  <span v-for="(tech, i) in position.tech" :key="i" class="amber--text text--darken-2">
+                    {{ tech }}
+                    <span class="blue--text text--lighten-2 text-caption mx-1 pt-2">
+                    |
+                    </span>
+                  </span>
+
+                </v-card-title>
+                <v-card-text>
+                  <span class="blue--text text--lighten-2 font-weight-bold">{{ position.highlight }}</span>
+                  {{ position.details }}
+                </v-card-text>
+              </v-card>
+            </v-timeline-item>
+
+          </v-timeline>
 
         </v-col>
       </section>
@@ -69,13 +105,20 @@
     created() {},
     destroyed() {},
     mounted() {},
-    computed: {},
+    computed: {
+      positions() {
+        return this.$store.state.positions
+      }
+    },
     watch: {},
     methods: {}
   }
 </script>
 
 <style scoped>
+>>>.theme--dark.v-timeline .v-timeline-item__dot {
+  background: #546E7A
+}
 .pos-rel {
   position: relative;
 }
