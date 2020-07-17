2dcf173d3bfc53bcdf83a4931e1d27d1cd256329
Matthew Esterberg <purebias@gmail.com>
2020-07-17 17:55:44 -0400

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  projectId: 'project-bluefire',
   storageBucket: "project-bluefire.appspot.com",
   messagingSenderId: "342995548873",
   appId: "1:342995548873:web:1c76e4fc07b8623e2c2b18"
