643e4f0a65b83bb20c9a64975d8a96224b146980
Matthew Esterberg <purebias@gmail.com>
2020-07-18 00:45:40 -0400

    Update ActivityPanels.vue
    Added code icon and messages for each commit.  Added commiter icons and reworked the PR header a bit to make it more reader-friendly. =)

--- src/components/GitHub/ActivityPanels.vue
+++ src/components/GitHub/ActivityPanels.vue
@@ -19,11 +19,16 @@
           <v-sheet class="col col-sm-2 col-md-1 ml-2 mb-2 transparent elevation-0">
             <p class="pa-0 ma-0 subtitle-2 c-grey-text ">Contributor:</p>
             <p class="pa-0 ma-0 overline amber--text text--darken-2"> {{ pull.user.login }}</p>
-            <p class="pa-0 ma-0 overline blue--text text--lighten-2 "> {{ daysAgo(pull.merged_at)  }} </p>
+            <p class="pa-0 ma-0 overline blue--text text--lighten-2"> {{ daysAgo(pull.merged_at)  }} </p>
           </v-sheet>
 
           <v-sheet class="col col-sm-9">
-            <p class="ma-0 pa-0 subtitle-2 c-grey-text">PR#{{ pull.number }}:  {{ pull.title }}</p>
+            <p class="ma-0 pa-0 subtitle-2 c-grey-text">
+              <v-icon size="20" class="mr-1 blue--text text--lighten-2">
+                fa-code-branch
+              </v-icon>
+              <span class="caption">({{ pull.number }})</span>  {{ pull.title }}
+            </p>
           </v-sheet>
         </v-row>
       </v-expansion-panel-header>
@@ -39,7 +44,15 @@
           </v-avatar>
 
           <v-sheet class="col col-sm-9">
-            <p class="ma-0 pa-0 subtitle-2 c-grey-text"> {{ daysAgo(message.commit.committer.date) }}...{{ message.commit.message }}</p>
+            <p class="ma-0 pa-0 subtitle-2 c-grey-text sen">
+              <v-icon size="10" class="blue--text text--lighten-2">
+                fa-code-branch
+              </v-icon>
+              <!--<span class="caption">
+                ({{ daysAgo(message.commit.committer.date) }})
+              </span>-->
+              {{ message.commit.message }}
+            </p>
           </v-sheet>
         </v-row>
 
