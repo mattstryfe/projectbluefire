c782e5ae0c0b1b98af94f704113941763f6aeb08
Matthew Esterberg <purebias@gmail.com>
2022-01-03 19:34:17 -0500

    added orderBy to recentLocs
    Although it doesn't seem to work as intended.  TODO: need to pull recentLoc query up to state so it can be triggered when user searches for new zip.

--- src/services/DWFServices.js
+++ src/services/DWFServices.js
@@ -1,9 +1,9 @@
-import { collection, getDocs, limit, query } from 'firebase/firestore/lite';
+import { collection, getDocs, limit, query, orderBy } from 'firebase/firestore/lite';
 import db from '../firebaseConfig'
 
 export async function getAllRecentLocations(count) {
   const recentLocationsRef = collection(db, 'geo')
-  const q = await query(recentLocationsRef, limit(count))
+  const q = await query(recentLocationsRef, orderBy('date_added'), limit(count))
   const recentLocationsSnapshot = await getDocs(q)
   const locArray = []
 
