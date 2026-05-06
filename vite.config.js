0c0c77a76e4b65ad2404f5a898ec649b2ebd4e55
Matthew Esterberg <purebias@gmail.com>
2026-05-06 08:29:55 -0400

    Update vite.config.js (#130)

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  build: {
+    cssMinify: 'esbuild'
+  },
   resolve: {
     alias: [
       {
